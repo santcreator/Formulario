784c5effa21f354c8af7e8766483e32dc7326d94
santcreator <39709346+santcreator@users.noreply.github.com>
2018-07-27 20:02:48 -0300

    Update validacaoFormulario.php

--- Formulário Com Validação/validacaoFormulario.php
+++ Formulário Com Validação/validacaoFormulario.php
@@ -4,19 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Validação de Formulários</title>
+    <title>Formulário com validação e campos obrigatorios</title>
+    <style>
+        .error{color:#FF0000;}
+    </style>
 </head>
 <body>
+
     <?php
+        $nameErr = $emailErr = $genderErr = $websiteErr = "";
         $name = $email = $gender = $comment = $website = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $name = test_input($_POST["name"]);
-            $email = test_input($_POST["email"]);
-            $website = test_input($_POST["website"]);
-            $comment = test_input($_POST["comment"]);
-            $gender = test_input($_POST["gender"]);
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required";
+            } else {
+                $name = test_input($_POST["name"]);
+            }
+
+            if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+            } else {
+                $email = test_input($_POST["email"]);
+            }
+
+            if (empty($_POST["website"])) {
+                $website = "";
+            } else {
+                $website = test_input($_POST["website"]);
+            }
+
+            if (empty($_POST["comment"])) {
+                $comment = "";
+            } else {
+                $comment = test_input($_POST["comment"]);
+            }
+
+            if (empty($_POST["gender"])) {
+                $genderErr = "Gender is required";
+            } else {
+                $gender = test_input($_POST["gender"]);
+            }
         }
+
         function test_input($data){
             $data = trim($data);
             $data = stripslashes($data);
@@ -24,31 +54,41 @@
             return $data;
         }
     ?>
+
     <fieldset>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            Name: <input type="text" name="name"><br><br>
-            E-mail: <input type="email" name="email"><br><br>
-            Website: <input type="text" name="website"><br><br>
-            Comment: <textarea name="comment" cols="40" rows="5"></textarea><br><br>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            Name: <input type="text" name="name">
+            <span class="error">* <?php echo $nameErr;?></span>
+            <br><br>
+            Email: <input type="email" name="email">
+            <span class="error">* <?php echo $emailErr;?></span>
+            <br><br>
+            Website: <input type="text" name="website">
+            <span class="error"><?php echo $websiteErr;?></span>
+            <br><br>
+            Comment: <textarea name="comment" cols="40" rows="5"></textarea>
+            <br><br>
             Gender:
             <input type="radio" name="gender" value="female">Female
-            <input type="radio" name="gender" value="Male">Male
+            <input type="radio" name="gender" value="male">Male
             <input type="radio" name="gender" value="other">Other
+            <span class="error">* <?php echo $genderErr;?></span>
             <br><br>
-            <input type="submit" value="Submit" name="submit">
+            <input type="submit" name="submit" value="Submit">
         </form>
     </fieldset>
+
     <?php
-        echo "<h2>Your Input:</h2>";
-        echo $name;
-        echo "<br>";
-        echo $email;
-        echo "<br>";
-        echo $website;
-        echo "<br>";
-        echo $comment;
-        echo "<br>";
-        echo $gender;
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
     ?>
 </body>
 </html>
